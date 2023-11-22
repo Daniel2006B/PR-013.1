cc30fb5846a033aa7a87254a0ed6e0632a3a948f
Danylo <Danylo.Brudnyi.RI.2023@lpnu.ua>
2023-11-22 15:19:20 +0200

    second test

---
+++ UnitTest013.1/UnitTest013.1.cpp
@@ -0,0 +1,29 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../PR-13.1/var.cpp"
+#include "../PR-13.1/dod.cpp"
+
+
+using namespace nsDod;
+using namespace nsVar;
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest131
+{
+	TEST_CLASS(UnitTest131)
+	{
+	public:
+
+		TEST_METHOD(TestMethod1)
+		{
+			x = 2;
+			n = 1;
+			a = 1.0;
+
+			dod();
+
+			Assert::AreEqual(a, 4, 0.1);
+		}
+	};
+}
